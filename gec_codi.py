91314e494feac7396f1a73688038a05e03729c9c
Gerard <gerard.escardo@estudiantat.upc.edu>
2023-02-26 20:28:30 +0100

    Ex1 a mitges

--- gec_codi.py
+++ gec_codi.py
@@ -6,10 +6,59 @@ import soundfile as sf
 T= 2.5                               # Durada de T segons
 fm=8000                              # Freqüència de mostratge en Hz
 fx=440                               # Freqüència de la sinusoide
+fy=4000                       # Freqüència ex1
+fz=50                         # Freqüència ex1
 A=4                                  # Amplitud de la sinusoide
 pi=np.pi                             # Valor del número pi
 L = int(fm * T)                      # Nombre de mostres del senyal digital
 Tm=1/fm                              # Període de mostratge
 t=Tm*np.arange(L)                    # Vector amb els valors de la variable temporal, de 0 a T
 x = A * np.cos(2 * pi * fx * t)      # Senyal sinusoidal
+y = A * np.cos(2 * pi * fy * t)  # Senyal sinusoidal ex1
+z = A * np.cos(2 * pi * fz * t)  # Senyal sinusoidal ex1
 sf.write('so_exemple1.wav', x, fm)   # Escriptura del senyal a un fitxer en format wav
+sf.write('so_ex1_4000.wav', y, fm)   # Escriptura del senyal a un fitxer en format wav
+sf.write('so_ex1_50.wav', z, fm)   # Escriptura del senyal a un fitxer en format wav
+
+Tx=1/fx                                   # Període del senyal
+Ty=1/fy                        # Període del senyal ex1
+Tz=1/fz                        # Període del senyal ex1
+Lsx=int(fm*5*Tx)                           # Nombre de mostres corresponents a 5 períodes de la sinusoide
+Lsy=int(fm*5*Ty)              # Nº mostres ex1
+Lsz=int(fm*5*Tz)             # Nº mostres ex1
+
+plt.figure(0)                             # Nova figura
+plt.plot(t[0:Lsx], x[0:Lsx])                # Representació del senyal en funció del temps
+plt.xlabel('t en segons')                 # Etiqueta eix temporal
+plt.title('5 periodes de la sinusoide')   # Títol del gràfic
+plt.show() 
+plt.figure(1)
+plt.plot(t[0:Lsy], x[0:Lsy])   # Representació ex1
+plt.xlabel('t en segons')                 # Etiqueta eix temporal
+plt.title('50 periodes de la sinusoide')   # Títol del gràfic
+plt.show() 
+plt.figure(2)
+plt.plot(t[0:Lsz], x[0:Lsz])   # Respresentació ex1
+plt.xlabel('t en segons')                 # Etiqueta eix temporal
+plt.title('0.5 periodes de la sinusoide')   # Títol del gràfic
+plt.show()                                # Visualització de l'objecte gràfic. 
+
+import sounddevice as sd      # Importem el mòdul sounddevice per accedir a la tarja de so
+sd.play(x, fm)                # Reproducció d'àudio
+
+from numpy.fft import fft     # Importem la funció fft
+N=5000                        # Dimensió de la transformada discreta
+X=fft(x[0 : Lsx], N)           # Càlcul de la transformada de 5 períodes de la sinusoide
+
+k=np.arange(N)                        # Vector amb els valors 0≤  k<N
+
+plt.figure(1)                         # Nova figura
+plt.subplot(211)                      # Espai per representar el mòdul
+plt.plot(k,abs(X))                    # Representació del mòdul de la transformada
+plt.title(f'Transformada del senyal de Ls={Lsx} mostres amb DFT de N={N}')   # Etiqueta del títol
+plt.ylabel('|X[k]|')                  # Etiqueta de mòdul
+plt.subplot(212)                      # Espai per representar la fase
+plt.plot(k,np.unwrap(np.angle(X)))    # Representació de la fase de la transformad, desenroscada
+plt.xlabel('Index k')                 # Etiqueta de l'eix d'abscisses 
+plt.ylabel('$\phi_x[k]$')             # Etiqueta de la fase en Latex
+plt.show()                            # Per mostrar els grafics
